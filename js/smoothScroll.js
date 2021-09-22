711a14028509d347a22f886039713289f4ee512c
kamshir <iammaksim2000shirshikov@gmail.com>
2021-09-22 22:54:46 +0300

    Smooth & Tabs"

---
+++ js/smoothScroll.js
@@ -0,0 +1,16 @@
+const linkHead = document.querySelectorAll('.menu-list__link')
+const mainScroll = document.querySelectorAll('.main__scroll')
+const newArray = [...linkHead, mainScroll]
+
+newArray.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault()
+
+    const ID = e.target.getAttribute('href').substr(1)
+
+    document.getElementById(ID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+})
